a4c030af132b394fa33cee048191e4ed640f2328
LouieMorais <louie.laicos@gmail.com>
2025-06-18 09:10:03 +0100

    README created

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Housarmony - Soon</Text>
+      <Text>HouseTasker - Soon</Text>
       <StatusBar style="auto" />
     </View>
   )
